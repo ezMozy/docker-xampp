fb6e244ad0b15ba7a65c9831925e10830b2cf0c7
st10511 <st10511@iismarconipieralisi.it>
2025-02-11 08:41:30 +0000

    completata chatroom

--- src/login.php
+++ src/login.php
@@ -1,3 +1,9 @@
 <?php
-echo "eseguito da php: Ciao, mondo!";
-?>
+
+if($_GET != null)
+    echo $_GET['username'];
+    echo $_GET['pw'];
+
+else if ($_POST != null)
+    echo $_POST['username'];
+    echo $_POST['pw'];
